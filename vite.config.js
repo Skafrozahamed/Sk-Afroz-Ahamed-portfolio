0fb1a2e93b8d43dda0471a80d5eb134ca37b7a85
Sk Afroz Ahamed <92941563+Skafrozahamed@users.noreply.github.com>
2025-10-10 23:42:15 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Sk-Afroz-Ahamed-portfolio/', // Replace with your repository name
+  base: '/SK-Afroz-Ahamed-portfolio/',
   build: {
     outDir: 'dist',
     sourcemap: true,
